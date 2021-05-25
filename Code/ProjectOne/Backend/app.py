7f2ae2e2b561350c426442625bd1e21f5cffa0d9
Diero <dieter.roobrouck@howest.be>
2021-05-25 17:26:58 +0100

    io not defined

--- Code/ProjectOne/Backend/app.py
+++ Code/ProjectOne/Backend/app.py
@@ -1,13 +1,17 @@
 import re
 import time
-from datetime import datetime
-# from helpers.klasseOneWire import OneWire
+from datetime import datetime, timedelta
+import threading
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, send
+from flask import Flask, jsonify
+
 from repositories.DataRepository import DataRepository
 
-nummer = 1
 
 sensor_file_name = '/sys/bus/w1/devices/28-0216146ad6ee/w1_slave'
 sensorfile = open(sensor_file_name, 'r')
+
 
 def read_temp():
     for i, line in enumerate(sensorfile):
@@ -15,17 +19,45 @@ def read_temp():
             temp = int(line.strip('\n')[line.find('t=')+2:])/1000.0
     return temp
 
-try:
+
+# Code voor Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'geheim!'
+socketio = SocketIO(app, cors_allowed_origins="*",
+                    logger=False, engineio_logger=False, ping_timeout=1)
+
+CORS(app)
+
+
+@socketio.on_error()        # Handles the default namespace
+def error_handler(e):
+    print(e)
+
+
+print("**** Program started ****")
+
+# API ENDPOINTS
+
+
+@app.route('/')
+def hallo():
+    return "Server is running, er zijn momenteel geen API endpoints beschikbaar."
+
+
+@socketio.on('connect')
+def initial_connection():
+    print('A new client connect')
+    # # Send to the client!
+    nummer = 1
     nummer += 1
     print(nummer)
     temp = read_temp()
-    date = datetime.now()
+    date = datetime.now()+timedelta(hours=1)
     DataRepository.toevoegen_historiek(nummer, 1, temp, 0, date)
-    time.sleep(2)
+    emit('B2F_waarde_device', {'waarde': temp}, broadcast=True)
 
-except KeyboardInterrupt as e:
-    print(e)
 
-finally:
-    print("Ended")
-    sensorfile.close()
+    # sensorfile.close()
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0')
--- Code/ProjectOne/Frontend/script/app.js
+++ Code/ProjectOne/Frontend/script/app.js
@@ -1,2 +1,27 @@
 const lanIP = `${window.location.hostname}:5000`;
 const socket = io(`http://${lanIP}`);
+
+let htmlds18;
+
+const listenToSocket = function () {
+    socket.on("connected", function () {
+      console.log("verbonden met socket webserver");
+    });
+    socket.on("B2F_waarde_device", function (jsonObject) {
+        console.log("Dit is de Waarde");
+        console.log(jsonObject);
+      });
+}
+
+//#region ***  Init / DOMContentLoaded                  ***********
+const init = function () {
+    console.log('DOM geladen')
+    // Get some DOM, we created empty earlier.
+    htmlds18 = document.querySelector('.js-data');
+    console.log(htmlds18);
+    listenToSocket();
+    //deze code wordt gestart vanaf index.html
+  };
+  
+  document.addEventListener('DOMContentLoaded', init);
+  //#endregion
